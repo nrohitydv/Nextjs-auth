18458756f183bda0553ba35dcae56460d5386daf
Rohit <rohityadav787898@gmail.com>
2023-08-02 20:22:08 +0545

    login and sigup created

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -5,26 +5,29 @@ import { useRouter } from "next/navigation";
 import { axios } from 'axios'
 
 
-function LoginPage() {
+function page() {
     const [user, setUser] = React.useState({
         email: '',
         password: '',
+        username: '',
     })
-    const onLogin = async () => {
+    const onSignup = async () => {
 
     }
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
-            <h1>Login</h1>
+            <h1>Register</h1>
             <hr />
-
+            <label htmlFor="username">username</label>
+            <input className="border rounded-md p-2 focus:outline-none focus:ring focus:border-blue-300" id="username" type="text" value={user.username} onChange={(e) => setUser({ ...user, username: e.target.value })} placeholder="username" />
+            <label htmlFor="email">email</label>
             <input className="border rounded-md p-2 focus:outline-none focus:ring focus:border-blue-300" id="email" type="text" value={user.email} onChange={(e) => setUser({ ...user, email: e.target.value })} placeholder="email" />
             <label htmlFor="password">password</label>
             <input className="border rounded-md p-2 focus:outline-none focus:ring focus:border-blue-300" id="password" type="password" value={user.password} onChange={(e) => setUser({ ...user, password: e.target.value })} placeholder="password" />
-            <button onClick={onLogin} className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:ring focus:border-blue-300">Signup</button>
-            <Link href='/signup'>Want to Register</Link>
+            <button onClick={onSignup} className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:ring focus:border-blue-300">Signup</button>
+            <Link href='/login'>Visit Login</Link>
         </div>
     )
 }
 
-export default LoginPage
+export default page
