feb6aa74edf8ce50db629b4f75e8f34f30ae10ba
Rohit <rohityadav787898@gmail.com>
2023-08-03 21:17:46 +0545

    simple used react hooks

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,33 +1,70 @@
-"use client";
-import Link from "next/link";
-import React from "react";
-import { useRouter } from "next/navigation";
-import { axios } from 'axios'
+'use client';
+import Link from 'next/link';
+import React, { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+import { axios } from 'axios';
 
+function SignupPage() {
+  const router = useRouter();
 
-function page() {
-    const [user, setUser] = React.useState({
-        email: '',
-        password: '',
-        username: '',
-    })
-    const onSignup = async () => {
-
+  const [user, setUser] = React.useState({
+    email: '',
+    password: '',
+    username: '',
+  });
+  const [buttonDisabled, setButtonDisabled] = React.useState(false);
+  const onSignup = async () => {};
+  useEffect(() => {
+    if (
+      user.email.length > 0 &&
+      user.password.length > 0 &&
+      user.username.length > 0
+    ) {
+      setButtonDisabled(false);
+    } else {
+      setButtonDisabled(true);
     }
-    return (
-        <div className="flex flex-col items-center justify-center min-h-screen py-2">
-            <h1>Register</h1>
-            <hr />
-            <label htmlFor="username">username</label>
-            <input className="border rounded-md p-2 focus:outline-none focus:ring focus:border-blue-300" id="username" type="text" value={user.username} onChange={(e) => setUser({ ...user, username: e.target.value })} placeholder="username" />
-            <label htmlFor="email">email</label>
-            <input className="border rounded-md p-2 focus:outline-none focus:ring focus:border-blue-300" id="email" type="text" value={user.email} onChange={(e) => setUser({ ...user, email: e.target.value })} placeholder="email" />
-            <label htmlFor="password">password</label>
-            <input className="border rounded-md p-2 focus:outline-none focus:ring focus:border-blue-300" id="password" type="password" value={user.password} onChange={(e) => setUser({ ...user, password: e.target.value })} placeholder="password" />
-            <button onClick={onSignup} className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:ring focus:border-blue-300">Signup</button>
-            <Link href='/login'>Visit Login</Link>
-        </div>
-    )
+  }, [user]);
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1>Register</h1>
+      <hr />
+      <label htmlFor="username">username</label>
+      <input
+        className="border rounded-md p-2 focus:outline-none focus:ring focus:border-blue-300 text-black "
+        id="username"
+        type="text"
+        value={user.username}
+        onChange={(e) => setUser({ ...user, username: e.target.value })}
+        placeholder="username"
+      />
+      <label htmlFor="email">email</label>
+      <input
+        className="border rounded-md p-2 focus:outline-none focus:ring focus:border-blue-300 text-black"
+        id="email"
+        type="text"
+        value={user.email}
+        onChange={(e) => setUser({ ...user, email: e.target.value })}
+        placeholder="email"
+      />
+      <label htmlFor="password">password</label>
+      <input
+        className="border rounded-md p-2 focus:outline-none focus:ring focus:border-blue-300 text-black"
+        id="password"
+        type="password"
+        value={user.password}
+        onChange={(e) => setUser({ ...user, password: e.target.value })}
+        placeholder="password"
+      />
+      <button
+        onClick={onSignup}
+        className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:ring focus:border-blue-300"
+      >
+        {buttonDisabled ? 'No Signup' : 'Signup'}
+      </button>
+      <Link href="/login">Visit Login</Link>
+    </div>
+  );
 }
 
-export default page
+export default SignupPage;
