a01d7dd9bedec24b9cde8793fc146154a29cc3d1
Rohit <rohityadav787898@gmail.com>
2023-08-03 23:34:18 +0545

    problem solved and able to sign up

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -2,7 +2,7 @@
 import Link from 'next/link';
 import React, { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
-import { axios } from 'axios';
+import axios from 'axios';
 
 function SignupPage() {
   const router = useRouter();
@@ -13,7 +13,17 @@ function SignupPage() {
     username: '',
   });
   const [buttonDisabled, setButtonDisabled] = React.useState(false);
-  const onSignup = async () => {};
+  const [loading, setLoading] = React.useState(false);
+  const onSignup = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.post('/api/users/signup', user);
+      console.log('Signup success', response.data);
+      router.push('/login');
+    } catch (error: any) {
+      console.log('signup failed', error.message);
+    }
+  };
   useEffect(() => {
     if (
       user.email.length > 0 &&
@@ -27,7 +37,7 @@ function SignupPage() {
   }, [user]);
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <h1>Register</h1>
+      <h1>{loading ? 'Processing' : 'Register'}</h1>
       <hr />
       <label htmlFor="username">username</label>
       <input
