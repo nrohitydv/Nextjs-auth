89f0825f2dd9defb1552bb934ea307ff230c3969
Rohit <rohityadav787898@gmail.com>
2023-08-06 14:43:21 +0545

    able to get user id on profile page

--- src/app/api/users/me/route.ts
+++ src/app/api/users/me/route.ts
@@ -6,4 +6,12 @@ import { connect } from '@/dbConfig/dbConfig';
 
 connect();
 
-
+export async function GET(request: NextRequest) {
+  try {
+    const userId = await getDataFromToken(request);
+    const user = await User.findOne({ _id: userId }).select('-password');
+    return NextResponse.json({ message: 'User found', data: user });
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 400 });
+  }
+}
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -2,9 +2,11 @@
 import axios from 'axios';
 import Link from 'next/link';
 import { toast } from 'react-hot-toast';
+import React, { useState } from 'react';
 import { useRouter } from 'next/navigation';
 export default function ProfilePage() {
   const router = useRouter();
+  const [data, setData] = useState('nothing');
 
   const logout = async () => {
     try {
@@ -18,17 +20,37 @@ export default function ProfilePage() {
     }
   };
 
+  //to get user details
+  const getuserDetails = async () => {
+    const res = await axios.get('/api/users/me');
+    console.log(res.data);
+    setData(res.data.data._id);
+  };
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2  ">
       <h1>Profile</h1>
       <hr />
       <p>Profile page</p>
+      <h2 className="p-1 rounded bg-green-500 ">
+        {data === 'nothing' ? (
+          'Nothing'
+        ) : (
+          <Link href={`/profile/${data}`}>{data}</Link>
+        )}
+      </h2>
       <hr />
       <button
         onClick={logout}
         className="bg-blue-500 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
       >
         Logout
+      </button>
+      <button
+        onClick={getuserDetails}
+        className="bg-green-800 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      >
+        GetUser Details
       </button>
     </div>
   );
