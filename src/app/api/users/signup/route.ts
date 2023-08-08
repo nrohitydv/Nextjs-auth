0748068c2b602150eb8d21ca02d80153a2efc864
Rohit <rohityadav787898@gmail.com>
2023-08-08 21:24:26 +0545

    mailer connected to signup page

--- src/app/api/users/signup/route.ts
+++ src/app/api/users/signup/route.ts
@@ -2,6 +2,7 @@ import { connect } from '@/dbConfig/dbConfig';
 import User from '@/models/userModal';
 import { NextRequest, NextResponse } from 'next/server';
 import bcryptjs from 'bcryptjs';
+import { sendEmail } from '@/helpers/mailer';
 
 connect();
 
@@ -29,6 +30,13 @@ export async function POST(request: NextRequest) {
     });
     const savedUser = await newUser.save();
     console.log(savedUser);
+    //verification email
+    await sendEmail({
+      email,
+      emailType: 'VERIFY',
+      userId: savedUser._id,
+    });
+
     return NextResponse.json({
       message: 'User created successfully',
       success: true,
--- src/helpers/mailer.ts
+++ src/helpers/mailer.ts
@@ -3,7 +3,7 @@ import User from '@/models/userModal';
 import bcryptjs from 'bcryptjs';
 
 //send mail
-export const sendMail = async ({ email, emailType, userId }: any) => {
+export const sendEmail = async ({ email, emailType, userId }: any) => {
   try {
     //create a hashed token
     const hashedToken = await bcryptjs.hash(userId.toString(), 10);
@@ -39,11 +39,15 @@ export const sendMail = async ({ email, emailType, userId }: any) => {
         process.env.domain
       }/verifyemail?token=${hashedToken}">here</a> to ${
         emailType === 'VERIFY' ? 'Verify your email' : 'Reset your password'
-      }</p>
+      }
+      or copy and paste the link below in your browser.</br>${
+        process.env.domain
+      }/verifyemail?token=${hashedToken}
+      </p>
       `,
     };
 
-    const mailResponse = await transport.sendMail(mailOptions);
+    const mailResponse = await transport.sendEmail(mailOptions);
     return mailResponse;
   } catch (error: any) {
     throw new error.message();
